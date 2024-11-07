d00cf24153936c985aeed168fd6459798557623e
iolandavm <iolandamassaranduba@gmail.com>
2024-11-06 17:15:31 -0700

    Add files via upload

---
+++ lab2.js
@@ -0,0 +1,25 @@
+wolf.addEventListener("click",function(){
+console.log('wolf')
+document.getElementById('animal').src="wolf.webp"
+
+})
+moose.addEventListener("click",function(){
+    console.log('moose')
+    document.getElementById('animal').src="moose.webp"
+    
+    })
+elk.addEventListener("click",function(){
+        console.log('elk')
+        document.getElementById('animal').src="elk.webp"
+        
+        })
+bear.addEventListener("click",function(){
+console.log('bear')
+document.getElementById('animal').src="bear.webp"
+            
+})
+sheep.addEventListener("click",function(){
+    console.log('sheep')
+    document.getElementById('animal').src="sheep.webp"
+    
+    })
